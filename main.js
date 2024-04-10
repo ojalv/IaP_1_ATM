3e48080a25e78a23d472c30c9c801f64b492a595
alvaro <oj.alv99@gmail.com>
2024-04-10 15:21:10 -0300

    v0.2 - comentario reubicado

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ if (saldo < 0 || retiro < 0) {
   console.log(`ERROR: monto negativo`);
 } else if (isNaN(saldo) || isNaN(retiro)) {
   console.log(`ERROR: NaN`);
+  //Salida
 } else if (saldo < retiro) {
   console.log(`saldo insuficiente para realizar el retiro`);
 } else if (saldo > retiro) {
   saldo = saldo - retiro;
-  //Salida
   console.log(`retiro: $${retiro}`);
   console.log(`saldo: $${saldo}`);
 }
