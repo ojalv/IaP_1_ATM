c92e6e2746c287af12a67049c06eae6e6075ec30
alvaro <oj.alv99@gmail.com>
2024-04-10 15:12:50 -0300

    v0.2 - validacion de datos para saldo y retiro, mensaje de saldo insuficiente

--- main.js
+++ main.js
@@ -3,18 +3,15 @@ let saldo = 500;
 let nombreUsuario = "Pepe";
 let retiro = 50;
 //Proceso
-if (saldo < 0) {
-  console.log(
-    `ERROR: saldo invalido, el saldo no puede ser un numero negativo`
-  );
-} else if (isNaN(saldo)) {
-  console.log(`ERROR: saldo invalido, el saldo no es un numero`);
+if (saldo < 0 || retiro < 0) {
+  console.log(`ERROR: monto negativo`);
+} else if (isNaN(saldo) || isNaN(retiro)) {
+  console.log(`ERROR: NaN`);
+} else if (saldo < retiro) {
+  console.log(`saldo insuficiente para realizar el retiro`);
 } else if (saldo > retiro) {
   saldo = saldo - retiro;
-
   //Salida
   console.log(`retiro: $${retiro}`);
   console.log(`saldo: $${saldo}`);
-} else if (saldo < retiro) {
-  console.log(`saldo insuficiente para realizar el retiro`);
 }
