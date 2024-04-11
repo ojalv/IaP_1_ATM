d0c043dad13b8815d18c347b04384438b30e8888
alvaro <oj.alv99@gmail.com>
2024-04-11 15:04:31 -0300

    v1.2 - alert para errores

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let nombreUsuario = "Pepe";
 let retiro = prompt("Ingrese el monto de su extraccion");
 //Proceso
 if (saldo < 0 || retiro < 0) {
-  console.log(`ERROR: monto negativo`);
+  alert(`ERROR: monto negativo`);
 } else if (isNaN(saldo) || isNaN(retiro)) {
-  console.log(`ERROR: NaN`);
+  alert(`ERROR: NaN`);
   //Salida
 } else if (saldo < retiro) {
   alert(`saldo insuficiente para realizar el retiro`);
