7e14e07afac4c4e6686c277ba2dfb71d50085e38
alvaro <oj.alv99@gmail.com>
2024-04-11 15:40:32 -0300

    v2.1 - Mostrar mensajes de error; agregado error de retiro igual a cero

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ let retiro;
 
 while (retiro == undefined || retiro <= 0 || isNaN(retiro) || retiro == "") {
   retiro = prompt("Ingrese el monto de su extraccion");
-}
-//Proceso
-if (saldo < 0 || retiro < 0) {
-  alert(`ERROR: monto negativo`);
-} else if (isNaN(saldo) || isNaN(retiro) || retiro == "") {
-  alert(`ERROR: NaN`);
+  //Proceso
+  if (saldo < 0 || retiro < 0) {
+    alert(`ERROR: monto negativo`);
+  } else if (isNaN(saldo) || isNaN(retiro) || retiro == "") {
+    alert(`ERROR: NaN`);
+  } else if (retiro == 0) {
+    alert(`ERROR: monto de extraccion igual a 0`);
+  }
 }
 //Salida
-else if (saldo < retiro) {
+if (saldo < retiro) {
   alert(`saldo insuficiente para realizar el retiro`);
 } else if (saldo >= retiro) {
   saldo = saldo - retiro;
