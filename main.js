3b7da62dee9060f92d4eab67e3ba56aaaea20334
alvaro <oj.alv99@gmail.com>
2024-04-11 17:03:45 -0300

    v2.3 - Mejora de mensajes para errores para retroalimentacion del usuario

--- main.js
+++ main.js
@@ -7,11 +7,20 @@ while (retiro == undefined || retiro <= 0 || isNaN(retiro) || retiro == "") {
   retiro = prompt("Ingrese el monto de su extraccion");
   //Proceso
   if (saldo < 0 || retiro < 0) {
-    alert(`ERROR: monto negativo`);
+    //ERROR: monto negativo
+    alert(
+      `Hola ${nombreUsuario}!\nParece que estas intentando extraer un numero negativo\nEsto no es posible, por favor ingresa un numero valido.`
+    );
   } else if (isNaN(saldo) || isNaN(retiro) || retiro == "") {
-    alert(`ERROR: NaN`);
+    //ERROR: monto NaN
+    alert(
+      `Hola ${nombreUsuario}!\nParece que ingresaste un caracter por error\nEsto no es valido, por favor ingresa un numero.`
+    );
   } else if (retiro == 0) {
-    alert(`ERROR: monto de extraccion igual a 0`);
+    //ERROR:monto de extraccion igual a 0
+    alert(
+      `Hola ${nombreUsuario}!\nParece que estas intentando extraer $0\nEsto no es posible, por favor ingresa un monto mayor a cero.`
+    );
   }
 }
 //Salida
