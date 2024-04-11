1bd464664d23aa242b094dc51216cd240abad3f5
alvaro <oj.alv99@gmail.com>
2024-04-11 17:25:28 -0300

    v3.0 - validar el retiro y mostrar los errores asociados se realizan con una funcion

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
+function validarMontoRetiro(retiro) {
+  //Esta funcion valida que el dato ingresado sea un numero, en caso contrario le informa al usuario el tipo de error
+  while (retiro == undefined || retiro <= 0 || isNaN(retiro) || retiro == "") {
+    retiro = prompt("Ingrese el monto de su extraccion");
+    if (saldo < 0 || retiro < 0) {
+      //ERROR: monto negativo
+      alert(
+        `Hola ${nombreUsuario}!\nParece que estas intentando extraer un monto negativo\nEsto no es posible, por favor ingresa un monto valido.`
+      );
+    } else if (isNaN(saldo) || isNaN(retiro) || retiro == "") {
+      //ERROR: monto NaN
+      alert(
+        `Hola ${nombreUsuario}!\nParece que ingresaste un caracter por error\nEsto no es valido, por favor ingresa un monto valido.`
+      );
+    } else if (retiro == 0) {
+      //ERROR:monto de extraccion igual a 0
+      alert(
+        `Hola ${nombreUsuario}!\nParece que estas intentando extraer $0\nEsto no es posible, por favor ingresa un monto mayor a cero.`
+      );
+    }
+  }
+  return retiro;
+}
+
 //Entrada
 let saldo = 500;
 let nombreUsuario = "Pepe";
 let retiro;
 
-while (retiro == undefined || retiro <= 0 || isNaN(retiro) || retiro == "") {
-  retiro = prompt("Ingrese el monto de su extraccion");
-  //Proceso
-  if (saldo < 0 || retiro < 0) {
-    //ERROR: monto negativo
-    alert(
-      `Hola ${nombreUsuario}!\nParece que estas intentando extraer un numero negativo\nEsto no es posible, por favor ingresa un numero valido.`
-    );
-  } else if (isNaN(saldo) || isNaN(retiro) || retiro == "") {
-    //ERROR: monto NaN
-    alert(
-      `Hola ${nombreUsuario}!\nParece que ingresaste un caracter por error\nEsto no es valido, por favor ingresa un numero.`
-    );
-  } else if (retiro == 0) {
-    //ERROR:monto de extraccion igual a 0
-    alert(
-      `Hola ${nombreUsuario}!\nParece que estas intentando extraer $0\nEsto no es posible, por favor ingresa un monto mayor a cero.`
-    );
-  }
-}
+//Proceso
+retiro = validarMontoRetiro(retiro);
+
 //Salida
 if (saldo < retiro) {
   alert(
