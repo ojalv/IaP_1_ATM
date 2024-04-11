aef99afb439e5159a5525826b8241134fd999913
alvaro <oj.alv99@gmail.com>
2024-04-11 15:26:41 -0300

    v1.4 - bugfix: validar si el monto esta vacio

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ let retiro = prompt("Ingrese el monto de su extraccion");
 //Proceso
 if (saldo < 0 || retiro < 0) {
   alert(`ERROR: monto negativo`);
-} else if (isNaN(saldo) || isNaN(retiro)) {
+} else if (isNaN(saldo) || isNaN(retiro) || retiro == "") {
   alert(`ERROR: NaN`);
-  //Salida
-} else if (saldo < retiro) {
+}
+//Salida
+else if (saldo < retiro) {
   alert(`saldo insuficiente para realizar el retiro`);
 } else if (saldo >= retiro) {
   saldo = saldo - retiro;
